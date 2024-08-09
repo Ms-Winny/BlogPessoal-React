38f06221ca3518987d08a4fb3287d7f9a4975709
Weida Winy <winnyweida@hotmail.com>
2024-08-09 17:39:58 -0300

    implementacao do cadastro e login

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+ 
+  
+
+  return (
+    <>
+        <div className="flex justify-center bg-indigo-900 text-white">
+          <div className="container flex flex-col items-center py-4">
+            <p className='text-xl font-bold'>Blog pessoal Weida Winny | Copyright </p>
+          </div>
+        </div>
+      </>
+  )
+}
+
+export default Footer
