7464a5c0ddb65335aaf7c8d917bc8da6b88711d0
Weida Winy <winnyweida@hotmail.com>
2024-08-09 15:06:59 -0300

    Exemplo hook useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import Home from './paginas/home/Home';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
- 
+  const [valor, setValor] = useState(0);
+
+  function handleClick() {
+    setValor(valor + 1);
+  }
+
   return (
-    <>
-      <Home/>
-    </>
+    <div>
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
   );
 }
 
