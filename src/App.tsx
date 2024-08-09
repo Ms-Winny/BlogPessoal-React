d71a108cd5775b5e14ccded7ecad95d8111accf5
Weida Winy <winnyweida@hotmail.com>
2024-08-09 15:35:08 -0300

    Sistema de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import Home from './paginas/home/Home.tsx';
 import './App.css';
 
@@ -6,7 +7,23 @@ import './App.css';
 function App() {
   return (
    <Home />
+=======
+import './App.css';
+import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+>>>>>>> Stashed changes
   );
 }
-
 export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import './Home.css';
 
@@ -8,6 +9,31 @@ function Home(){
             <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
         </>
     );
+=======
+import { useNavigate, Link } from 'react-router-dom'
+import React from 'react'
+
+
+
+const Home = () => {
+    let navigate = useNavigate()
+    return (
+
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
+
+        </div>
+
+    )
+>>>>>>> Stashed changes
 }
 
-export default Home;
+export default Home
