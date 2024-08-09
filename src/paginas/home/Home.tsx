34376e81918d059ef689bb2ad0011b6ed878aa1d
Weida Winy <winnyweida@hotmail.com>
2024-08-09 15:04:03 -0300

    Exemplo do uso do useEffect

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import './Home.css';
+import React, { useState, useEffect } from 'react';
 
-function Home(){
-    return (
-        <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
-        </>
-    );
+function Home() {
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState('');
+
+  useEffect(() => {
+    if (completed) {
+      setTarefa('Parabéns! Você concluiu a tarefa!');
+    }
+  }, [completed]);
+
+  return (
+    <div>
+      <h1>Tarefa</h1>
+      <h3>{tarefa}</h3>
+      <p>Conclua a tarefa</p>
+      <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
+    </div>
+  );
 }
 
 export default Home;
