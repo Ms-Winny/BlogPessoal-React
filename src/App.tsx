9c65454a0ebcae9fb62ff3a2ca57460e25398839
Weida Winy <winnyweida@hotmail.com>
2024-08-16 15:38:13 -0300

    criacao do crud das postagens

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas.tsx';
 import FormularioTema from './components/temas/formularioTema/FormularioTema.tsx';
 import DeletarTema from './components/temas/deletarTema/DeletarTema.tsx';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens.tsx';
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem.tsx';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem.tsx';
 
 
 function App() {
@@ -29,6 +32,10 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
